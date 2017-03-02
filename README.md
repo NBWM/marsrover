f4b7a5f33999ac90fc56bf6c713ef19710df1c10
Jeffrey <jeffrey@nbwm.nl>
2017-03-02 11:53:43 +0100

    Init

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # marsrover
+
+1. Install (https://getcomposer.org/)[Composer]
+2. Run `composer install` in the top folder of this project.
---
+++ test/MarsRoverTest.php
@@ -0,0 +1,8 @@
+<?php
+
+class MarsRoverTest extends PHPUnit_Framework_TestCase {
+
+	function test_true_is_true() {
+		$this->assertTrue(true);
+	}
+}
